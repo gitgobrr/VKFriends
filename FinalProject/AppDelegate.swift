d119b8929e5778539e0c382ee5b07ccafa16f3b2
Сергей <gitgobrr@gmail.com>
2023-06-13 18:02:36 +0300

    Remove storyboard. Set up initial view controller

--- FinalProject/AppDelegate.swift
+++ FinalProject/AppDelegate.swift
@@ -9,11 +9,19 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if #unavailable(iOS 13) {
+            self.window = UIWindow()
+            self.window?.backgroundColor = .white
+            let vc = ViewController()
+            self.window?.rootViewController = vc
+            self.window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
